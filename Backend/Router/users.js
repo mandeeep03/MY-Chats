f8540a7190e07d44b5b04947ba45ee37a1506154
mandeeep03 <mandeepbhatt52@gmail.com>
2026-01-01 09:04:31 +0530

    added dotenv and mongodb sever online

--- Backend/Router/users.js
+++ Backend/Router/users.js
@@ -2,7 +2,10 @@ const express =  require('express')
 const { handleUserSignUp } = require('../Controllers/users')
 const router = express.Router()
 
-router.post('/',handleUserSignUp)
+router
+    .route('/signup')
+    .post(handleUserSignUp)
+
 
 
 module.exports = router
--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,26 @@
 const express = require('express')
 const { connectMongoDB } = require("./connect");
 const userRouter = require('./Router/users')
+require('dotenv').config()
+const cors = require('cors')
 const app = express()
 
 //Puts data in body 
 app.use(express.urlencoded({extended:true}))
 
+
 //Connecting database 
-connectMongoDB("mongodb://127.0.0.1:27017/my-chat-db").then(()=>{
+connectMongoDB(process.env.MONGODB_URI)
+    .then(()=>{
     console.log("Databse Connected Succesfully")
-}).catch((err)=>{
+})
+    .catch((err)=>{
     console.log("Error for database is " + err)
 })
 
 app.use('/users',userRouter)
 
 //starting server
-app.listen('8000',()=>{
-    console.log(`server listining on http://localhost:8000`)
+app.listen(process.env.PORT,()=>{
+    console.log(`server listining on http://localhost:${process.env.PORT}`)
 })
